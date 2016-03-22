0df69de916a1a9617055c06f9a2003a7aba69d10
dyindude <dyindude@github.dyindude.net>
2016-03-22 13:48:02 -0500

    started warmup-2

---
+++ warmup-2/string_times.lua
@@ -0,0 +1,18 @@
+function string_times(str,n)
+  return string.rep(str,n)
+end
+
+values = {{'Hi',      2},
+          {'Hi',      3},
+          {'Hi',      1},
+          {'Hi',      0},
+          {'Hi',      5},
+          {'Oh Boy!', 2},
+          {'x',       4},
+          {'',        4},
+          {'code',    2},
+          {'code',    3}}
+
+for i,v in ipairs(values) do
+  print("string_times('" .. v[1] .. "," .. v[2] .. "'): " .. string_times(v[1],v[2]))
+end
